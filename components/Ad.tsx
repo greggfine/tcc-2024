d771c66975f733c76d41c45a846d95e86340cd23
Gregg Fine <gregg@greggfinedev.com>
2024-08-08 17:12:12 -0400

    Fixes all blog post image paths

--- components/Ad.tsx
+++ components/Ad.tsx
@@ -1,12 +1,17 @@
 import Image from "next/legacy/image";
 import styles from "styles/ad.module.scss";
+import { basePath } from "basePath";
 const Ad = ({ txt, imgSrc, imgWidth, imgHeight, href }) => {
   return (
     <div className={styles.Ad}>
       <a className={styles.Ad__txt} href={href}>
         <p>{txt}</p>
         <Image
-          src={imgSrc}
+          src={
+            process.env.NODE_ENV === "production"
+              ? `${basePath}${imgSrc}`
+              : `${imgSrc}`
+          }
           width={imgWidth}
           height={imgHeight}
           layout="responsive"
--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -1,15 +1,27 @@
 import styles from "styles/youtube.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import { basePath } from "basePath";
 
 const YouTube = ({ id, imgSrc, alt }) => {
   return (
     <div className={styles.YouTube}>
-      <Link href={`https://www.youtube.com/watch?v=${id}`} target="_blank" rel="noopener" 
+      <Link
+        href={`https://www.youtube.com/watch?v=${id}`}
+        target="_blank"
+        rel="noopener"
       >
-        <Image 
-       quality={50} 
-        src={imgSrc} alt={alt} width={400} height={225}></Image>
+        <Image
+          quality={50}
+          src={
+            process.env.NODE_ENV === "production"
+              ? `${basePath}${imgSrc}`
+              : `${imgSrc}`
+          }
+          alt={alt}
+          width={400}
+          height={225}
+        ></Image>
       </Link>
     </div>
   );
